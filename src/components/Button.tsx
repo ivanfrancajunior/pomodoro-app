9e408a24824d9047dfb933ee6051e9ef3b18a640
Ivan França <contato.ivanfrancajr@gmail.com>
2023-04-14 12:38:18 -0300

    feat: adicionado função de play e pause

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 interface Props {
+  
   text: string;
   onClick?: () => void;
   className?: string;
--- src/components/Timer/PomodoroTimer.tsx
+++ src/components/Timer/PomodoroTimer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useInterval } from '../../Hooks/useInterval'
 import { Timer } from '../Timer'
 import Button from '../Button'
@@ -11,19 +11,34 @@ interface Props {
 }
 const PomodoroTimer = (props: Props): JSX.Element => {
   const [mainTime, setMainTime] = React.useState(props.PomodoroTimer)
+
+  const [timeCount, setTimeCount] = React.useState(false)
+  const [working, setWorking] = React.useState(false)
+
+  useEffect(() => {
+    if (working) document.body.classList.add('working')
+  }, [working])
+
+  const configureWorking = () => {
+    setTimeCount(true)
+    setWorking(true)
+  }
+
   useInterval(() => {
     setMainTime(mainTime - 1)
-  }, 1000)
+  }, timeCount ? 1000 : null)
+
+
 
   return (
     <div className='pomodoro'>
       <div className='button-container'>
-        <Button text='Pomodoro' onClick={()=> console.log('cliquei')}></Button>
-        <Button text='Descanso' onClick={()=> console.log('cliquei')}></Button>
-        <Button text='Longo Descanso' onClick={()=> console.log('cliquei')}></Button>
-    </div>
+        <Button text='Pomodoro' onClick={configureWorking}></Button>
+        <Button text='Descanso' onClick={() => console.log('cliquei')}></Button>
+        <Button text='Longo Descanso' onClick={() => console.log('cliquei')}></Button>
+      </div>
       <Timer mainTime={mainTime} />
-      <Button text='clicas' onClick={()=> console.log('cliquei')}></Button>
+      <Button text={timeCount ? 'Pause': 'Play'} onClick={() => setTimeCount(!timeCount)}></Button>
     </div>
   )
 }
